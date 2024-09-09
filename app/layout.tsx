dcddf9f0eb2b5907a9b35439a58ada4d36dcdb33
Daryl <borismbakop611@gmail.com>
2024-09-09 04:21:17 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,12 +33,15 @@ export default function RootLayout({
   return (
     <html lang="en"  >
       
-      <body className={cn(
+      <body 
+      className={cn(
          GeistSans.variable,
          GeistMono.variable,
          AnekTelugu.variable,
         "flex flex-col font-sans min-h-screen "
-      )}>
+      )}
+      
+      >
            <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill=""
